c13a483c49ac6c76620d89069b2fa0ea34945d60
Tiyara-03 <tiara26dia37@gmail.com>
2024-06-07 07:28:28 +0530

    update in the pages
    Addition of new section i.e shop collection and footer, with their styling in style.css.

--- main/index.js
+++ main/index.js
@@ -1,12 +1,26 @@
-console.log("Console is working");
+// console.log("hello world");
 
-const sr = ScrollReveal({
-    distance: '65px',
-    duration: 2600,
-    delay: 450,
-    reset: true
+// const sr = ScrollReveal({
+//     distance: '65px',
+//     duration: 2600,
+//     delay: 450,
+//     reset: true
 
-})
+// })
 
-sr.reveal('.about', {delay: 200, origin: left});
-sr.reveal('.image', {delay: 200, origin: right});
+// sr.reveal('.about', {delay: 200, origin: 'left'});
+// sr.reveal('.image', {delay: 200, origin: 'right'});
+
+// let scroll = document.querySelector(".box");
+// let bckscroll = document.querySelector("#backbtn");
+// let forscroll = document.querySelector("#forwardbtn");
+
+// bckscroll.onclick = () => {
+//     console.log("btn is clicked");
+//     scroll.scrollLeft -= 350;
+// }
+
+// forscroll.onclick = () => {
+//     console.log("btn is clicked");
+//     scroll.scrollLeft += 350;
+// }
